4e5a2562822606d84f683a445ecce52ca5dc5894
AsafAsgard <105627317+AsafAsgard@users.noreply.github.com>
2023-06-15 13:22:10 +0300

    Logic improvements

--- Assets/LineOfSight/Scripts/LineOfSightScanner.cs
+++ Assets/LineOfSight/Scripts/LineOfSightScanner.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
 using Unity.Burst;
-using Unity.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -15,6 +13,7 @@ namespace LOS
     [RequireComponent(typeof(LineOfSightBase))]
     public class LineOnSightScanner : MonoBehaviour, ISightModule
     {
+        public enum ScanErrorType { Fixed, Curve }
         public string FieldName { get; } = "LOS";
 
         private LineOfSightParameters _parameters;
@@ -25,9 +24,14 @@ namespace LOS
 
 
         [Header("Noise Options")]
-        [SerializeField] private float noiseRadius = 0;
+        [SerializeField] private bool enableNoise;
+        [SerializeField] private float noiseMaxRadius = 0;
         [SerializeField] private float noiseMinDistance = 0;
-        //[SerializeField] private float errorChance = 0f;
+        [SerializeField] private float noiseMaxDistance = 0;
+
+        [Header("Identification Error")]
+        [SerializeField] private bool enableIdError;
+        [SerializeField] private float errorProbability = 0;
 
         [Header("Debug")]
         [SerializeField, Tooltip("There lines will be visible in Game Mode!")]
@@ -46,6 +50,15 @@ namespace LOS
         /// All relevant objects in cone of sight
         /// </summary>
         private List<GameObject> poiTargets;
+
+        public List<T> GetTargets<T>(Func<GameObject, T> converter)
+        {
+            return poiTargets
+                .Select(t => converter(t))
+                .ToList();
+        }
+        
+       
 
         public List<GameObject> GetTargets() => poiTargets;
 
@@ -96,31 +109,24 @@ namespace LOS
             isScanning = true;
             poiTargets.Clear();
             //Add targets by tag
+            GameObject entity;
             if (allEntities != null && _parameters.tagsDictionary != null)
             {
                 for (int i = 0; i < allEntities.Count; i++)
                 {
-                    GameObject entity = allEntities[i];
+                    entity = allEntities[i];
 
-                    if (entity == _parameters.root) continue;
-                    if (!_parameters.tagsDictionary.Keys.Contains(entity.tag)) continue;
+                    if (entity == _parameters.root) continue; // prevent seld id
+                    if (!_parameters.tagsDictionary.Keys.Contains(entity.tag)) continue; // ID-able tag
                     if (Vector3.Distance(transform.position, entity.transform.position) > _parameters.tagsDictionary[entity.tag]) continue;
+
                     if (TargetInSight(entity.transform))
                         poiTargets.Add(entity);
                 }
-                //foreach (LayerDetails layerDetail in parameters.detectionLayers)
-                //{
-                //    IEnumerable<GameObject> targetsInLayer = allEntities.Where(target => target.tag.Equals(layerDetail.targetTag));
-                //    IEnumerable<GameObject> targetsInRange = targetsInLayer.Where(entity => Vector3.Distance(transform.position, entity.transform.position) <= layerDetail.distance);
-                //    IEnumerable<GameObject> layerPOI = targetsInRange.Where(poi => TargetInSight(poi.transform));
-                //    poiTargets.AddRange(layerPOI);
-                //}
             }
-
-            //if (poiTargets.Contains(parameters.root))
-            //    poiTargets.Remove(parameters.root);
             isScanning = false;
         }
+
 
         /// <summary>
         /// Checks if a point is within the LOS angle and is not blocked by enviroment object (check by layer)
@@ -170,16 +176,17 @@ namespace LOS
 
         public Vector3 GetNoiseOffset(Vector3 pestLoc)
         {
-            float distanceToTarget = Vector3.Distance(transform.position, pestLoc);
-            if (distanceToTarget < noiseMinDistance)
+            if (!enableNoise)
                 return Vector3.zero;
-            else
+            float distanceToTarget = Vector3.Distance(transform.position, pestLoc);
+            if (distanceToTarget > noiseMinDistance)
             {
-                float radius = noiseRadius * distanceToTarget / _parameters.maxViewDistance;
-
-                Vector2 noise = Random.insideUnitCircle * radius;
+                float noiseRadiusModifier = Mathf.Clamp01(distanceToTarget / noiseMaxDistance);
+                
+                Vector2 noise = noiseMaxRadius * noiseRadiusModifier * Random.insideUnitCircle;
                 return new Vector3(noise.x, 0, noise.y); // should return the correct point
             }
+            return Vector3.zero;
         }
 
         #endregion
@@ -240,4 +247,10 @@ namespace LOS
     }
 }
 
+public struct TargetObject
+{
+    public string name;
+    public Vector3 position;
+    public bool alive;
+}
 
